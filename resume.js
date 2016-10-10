3bd584c6a80779b480536569abb9a89b815fa141
Dhruv Singhal <dhruv@singhal.me>
2016-10-10 15:50:01 -0400

    Updated resume for Fall 2016.

--- resume.js
+++ resume.js
@@ -4,9 +4,9 @@ setTimeout(function() {
   var phoneText = document.getElementById('phone');
   var contactInfo = document.getElementById('contact-info');
 
-  emailText.innerHTML = atob('ZGhydXZAc2luZ2hhbC5tZQ==');
-  addressText.innerHTML = atob('MTUxIERyeWRlbiBSb2FkLCBBcHQgNTIwLCBJdGhhY2Es' +
-    'IE5ZIDE0ODUw');
-  phoneText.innerHTML = atob('KzEgKDYwNykgMjc5LTU3Mjk=');
+  emailText.innerHTML = atob('cGFydGhzaW5naGFsQGJlcmtlbGV5LmVkdQ==');
+  addressText.innerHTML = atob('MjczMiBIYXN0ZSBTdHJlZXQsIEJlcmtlbGV5LCBDQSA5N' +
+    'DcwNA==');
+  phoneText.innerHTML = atob('KzEgKDUxMCkgNjc0LTQ2NTU=');
   contactInfo.style.display = 'block';
 }, 2000);
